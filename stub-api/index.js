09f74f6b0ef42f33c144f0a7052c4a43c6516518
Marcin Sirocki <marcinsirocki@gmail.com>
2018-06-30 12:56:13 +0200

    init

---
+++ stub-api/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const faker = require('faker');
+const {times} = require('lodash');
+
+const app = express();
+
+const generateFile = () => ({
+    author: {
+        avatar: faker.internet.avatar(),
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    },
+    name: faker.system.fileName(),
+    type: faker.system.fileType(),
+    ext: faker.system.fileExt(),
+    path: faker.system.filePath(),
+    semver: faker.system.semver(),
+    createDate: faker.date.past()
+});
+
+const files = times(100, generateFile);
+
+app.get('/api/files', (req, res) => {
+    res.send({files});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
